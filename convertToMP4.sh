061db3f6c4760ea75c8b9edfc874f2c242d470bd
Pranat Dayal <pranat.dayal@gmail.com>
2020-08-04 12:12:49 -0400

    Convert MKV to MP4 using ffmpeg

---
+++ convertToMP4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+dir=$1
+logfile=/disks/sgt/Movies/logfile.txt
+
+printf "**Beginning conversion now\n" >> $logfile
+printf "**Dir: $1\n\n" >> $logfile
+shopt -s globstar;
+
+for i in $dir/**/*.mkv; do
+    printf "Attempting $i\n" >> $logfile
+    #false 1
+    ffmpeg -i "$i" -codec copy -movflags +faststart "${i%.*}.mp4"
+    if [[ $? -gt 0 ]]; then
+        printf "[-]Conversion Failed - verify manually\n\n" >> $logfile
+    else
+        rm "$i"
+        printf "[+]looks good deleting $i\n\n" >> $logfile
+    fi
+done
+
